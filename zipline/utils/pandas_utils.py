f6d375bee0bc517d4672cc35ccdb609e1b5779ad
jfkirk <james.f.kirk@gmail.com>
2016-05-12 10:58:43 -0400

    BUG: Adds specific version check to pandas_utils

--- zipline/utils/pandas_utils.py
+++ zipline/utils/pandas_utils.py
@@ -2,6 +2,9 @@
 Utilities for working with pandas objects.
 """
 import pandas as pd
+from distutils.version import StrictVersion
+
+pandas_version = StrictVersion(pd.__version__)
 
 
 def explode(df):
@@ -16,8 +19,11 @@ def explode(df):
 try:
     # This branch is hit in pandas 17
     sort_values = pd.DataFrame.sort_values
-    july_5th_holiday_observance = lambda dtix: dtix[dtix.year != 2013]
 except AttributeError:
     # This branch is hit in pandas 16
     sort_values = pd.DataFrame.sort
+
+if pandas_version >= StrictVersion('0.17.1'):
+    july_5th_holiday_observance = lambda dtix: dtix[dtix.year != 2013]
+else:
     july_5th_holiday_observance = lambda dt: None if dt.year == 2013 else dt
