60a4d1853eedbdb08e5ce69d569cdcc0ad578b77
jfkirk <james.f.kirk@gmail.com>
2016-05-26 15:58:48 -0400

    MAINT: Rebase reconciliation

--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -1877,7 +1877,7 @@ class TestCapitalChanges(WithLogger,
 
     @classmethod
     def make_minute_bar_data(cls):
-        minutes = cls.env.minutes_for_days_in_range(
+        minutes = cls.trading_schedule.execution_minutes_for_days_in_range(
             pd.Timestamp('2006-01-03', tz='UTC'),
             pd.Timestamp('2006-01-09', tz='UTC')
         )
@@ -1889,14 +1889,14 @@ class TestCapitalChanges(WithLogger,
                     [10000] * len(minutes),
                     timedelta(minutes=1),
                     cls.sim_params,
-                    cls.env),
+                    cls.trading_schedule),
             },
             index=pd.DatetimeIndex(minutes),
         )
 
     @classmethod
     def make_daily_bar_data(cls):
-        days = cls.env.days_in_range(
+        days = cls.trading_schedule.execution_days_in_range(
             pd.Timestamp('2006-01-03', tz='UTC'),
             pd.Timestamp('2006-01-09', tz='UTC')
         )
@@ -1908,7 +1908,7 @@ class TestCapitalChanges(WithLogger,
                     [10000] * len(days),
                     timedelta(days=1),
                     cls.sim_params,
-                    cls.env),
+                    cls.trading_schedule),
             },
             index=pd.DatetimeIndex(days),
         )
